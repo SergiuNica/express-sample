3e8fa4ab68d4c73aaffbc75801509edf92a87359
Nica <sergiunica@gmail.com>
2019-04-09 13:46:48 -0400

    showing html pages from database and using styles

--- routes/root.js
+++ routes/root.js
@@ -1,8 +1,40 @@
 const router= require('express').Router();
+const db = require('../config/db.js');
 
-router.get('/', (req, res)=> {
-    return res.render('root', {title:'First express app'});
+// async function getUsers(){
+//     let connection = await db.getConnection();
+//     const rows = await connection.query("SELECT * FROM user");
+//     connection.end();
+//     return rows;
+// }
+
+async function getPage(pagekey){
+    let connection = await db.getConnection();
+    const rows = await connection.query("SELECT * FROM page WHERE pageKey = ?", [pagekey]);
+    connection.end();
+    return rows;
+}
+
+router.get('/', async (req, res)=> {
+    
+    let page = await getPage('home');
+    page = page[0];
+    //let users = await getUsers();
+    //console.log(page);
+    return res.render('root', {title: page.title, page: page});
 });
+
+router.get('/:page', async (req, res, next)=> {
+    let p= req.params.page.toLowerCase();
+    let page = await getPage(p);
+    if(page[0] !==undefined){
+    page = page[0];
+    return res.render('root', {title: page.title, page: page});
+    } else {
+        next();
+    }        
+});
+
 router.get('/', (req, res)=> res.send('Welcome Home!'));
 router.get('/hello', (req, res)=> res.send('Hey there!'));
 
