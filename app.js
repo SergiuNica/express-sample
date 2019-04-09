c344fb6151c3e6b8972b2a6e44f6a5678910bada
Nica <sergiunica@gmail.com>
2019-04-09 09:59:01 -0400

    new users folder with users.js file

--- app.js
+++ app.js
@@ -1,10 +1,44 @@
 const express = require('express');
+const path = require ('path');
+const cookieParser = require ('cookie-parser');
 const app = express();
 const port = 9000;
 
-app.get('/', (req, res)=> res.send('Hello!'));
-app.get('/home', (req, res)=> res.send('Welcome Home!'));
-app.get('/user/:username', (req, res)=> res.send('Welcome User! - '+ req.params.username));
+const usersRouter = require('./routes/users');
+
+app.use(cookieParser());
+
+// app.use((req, res, next) => {
+// res.cookie('cookies', 'yum');
+//     res.send(req.cookies.cookies);
+// next();
+// });
+
+app.use('/users', usersRouter);
+
+//app.use('/inc',express.static(path.join(__dirname, 'inc')));
+//app.get('/', (req, res)=> res.send('Hello!'));
+app.get('/hello', (req, res)=> res.send('Hey there!'));
+app.get('/users/:userId/:email', (req, res)=> {
+    let uid = parseInt(req.params.userId);
+    let email = req.params.email;
+    res.send(`user ${uid}'s email set to ${email}`);
+}    );
+
+app.get('/numbers/:from-:to', (req, res)=>{
+    let a = parseInt(req.params.from);
+    let b = parseInt(req.params.to);
+    let html = '';
+    if(a>b){
+        let t= a;
+        a=b;
+        b=t;
+    }
+    for (let x=a;x<=b;x++){
+        html+=`<div>${x}</div>`;
+    }
+    res.send(html);
+});
 
 app.listen(port, 
     function() {
